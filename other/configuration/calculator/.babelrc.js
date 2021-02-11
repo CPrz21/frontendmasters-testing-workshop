40da337799368e4b0bdad6ba72a024a0049497e6
CPrz21 <faselona@gmail.com>
2021-02-11 16:19:14 -0600

    configuring jest and babel

--- other/configuration/calculator/.babelrc.js
+++ other/configuration/calculator/.babelrc.js
@@ -1,11 +1,27 @@
-module.exports = {
-  presets: [['env', {modules: false}], 'react'],
-  plugins: [
-    'syntax-dynamic-import',
-    'transform-class-properties',
-    'transform-object-rest-spread',
-  ],
+const isTest = String(process.env.NODE_ENV) === 'test'
+
+// Babel needs to recieve it's config as a function on module.exports so we
+// have to wrap our object in a function
+const test = api => {
+
+  // Babel requires api.cache(true | false) to be set
+  api.cache(true)
+
+  return {
+    presets: [
+      ['@babel/preset-env', {modules: isTest ? 'commonjs' : false}],
+      '@babel/preset-react',
+    ],
+    plugins: [
+      'syntax-dynamic-import',
+      'transform-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+      isTest ? 'dynamic-import-node' : null,
+    ],
+  }
 }
+
+module.exports = test
 
 /*
 Solution snippets below
